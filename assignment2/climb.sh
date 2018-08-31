cb0ee4be758b7d9a621b7060957123d36b556d59
Daniel Sharifi <danish@kennen.ifi.uio.no>
2018-08-31 19:09:57 +0200

    Fixed climb. Now supports no args

--- assignment2/climb.sh
+++ assignment2/climb.sh
@@ -1,9 +1,13 @@
 climb() {
-  mssg=''
+  #climbs one step if no args are given
+  mssg='../'
   comm='../'
-  for ((i = 1; i <= $1; i++))
-  do
-    mssg=$mssg$comm
-  done
+
+  if [ ${1} ]; then
+    for ((i = 0; i < $1 - 1; i++));
+    do
+      mssg=$mssg$comm
+    done
+  fi
   eval "cd ${mssg}"
 }

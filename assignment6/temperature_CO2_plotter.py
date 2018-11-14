59485a9f7e459fd411b7ba73a8df0d051616af00
DSharifi <danielsharifi@outlook.com>
2018-11-14 22:34:47 +0100

    Done all tasks till 6.4

--- assignment6/temperature_CO2_plotter.py
+++ assignment6/temperature_CO2_plotter.py
@@ -2,38 +2,134 @@ import pandas as pd
 import numpy as np
 import argparse
 import os
+import matplotlib.pyplot as plt
+
+
+co2_by_country = pd.read_csv('CO2_by_country.csv', sep=',' )
+co2 = pd.read_csv('co2.csv', sep=',')
+temp = pd.read_csv('temperature.csv', sep=',')
+
+# Can give a false positive by default, as adressed here:
+# http://pandas-docs.github.io/pandas-docs-travis/#returning-a-view-versus-a-copy
+pd.options.mode.chained_assignment = None
+
+
+
+
+def plot_temperature(month, year_start, year_end, y_min, y_max):
+    """plots temperatue for a month over range of years
+    
+    Arguments:
+        month {str} -- name of month
+        month2 {str} -- name of other month
+
+        year_start {int} -- starting year
+        year_end {int} -- end year
+
+        y_min {float} -- y-axis minimum
+        y_max {float} -- y-axis maximum
+    
+    Returns:
+        pyplot -- plot of the temperature, according to input arguments.
+    """
+
+    temp_month = temp[["Year", month]]
+    temp_month = temp_month[(temp_month['Year'] >= year_start) &
+                            (temp_month['Year'] <= year_end)]
+
+    plt.close()
+   
+    ax = temp_month.plot(y = month, x = 'Year')
+    ax.set(xlabel="Year", ylabel="Temperature (C)")
+
+    plt.ylim(y_min, y_max)
+
+    return plt    
+
+
+def plot_CO2(year_start, year_end, y_min, y_max):
+    """plots CO2 levels over a range of years
+    
+    Arguments:
+        year_start {int} -- starting year
+        year_end {int} -- end year
+
+        y_min {float} -- y-axis minimum
+        y_max {float} -- y-axis maximum
+    
+    Returns:
+        pyplot -- plot of the CO2 levels, according to input arguments.
+    """
+
+    co2_plot = co2[(co2['Year'] >= year_start) &
+                     (co2['Year'] <= year_end)]
+    plt.close()
+    ax = co2_plot.plot(y='Carbon', x='Year', title="Increase of carbon levels")
+    ax.set(xlabel="Year", ylabel="CO2 Level")
+    plt.ylim(y_min, y_max)
+    
+
+    return plt
 
 
 
 
 
-def plot(time_file, compare_file, compare_t):
-    co2_by_country = pd.read_csv('CO2_by_country.csv')
-    co2 = pd.read_csv('co2.csv')
-    temp = pd.read_csv('temperature.csv')
+def plot_CO2_by_Country(lower_threshold, upper_threshold, year):
+    """Plots every country between given threshold on a bar chart
+    
+    Arguments:
+        lower_threshold {float} -- lower threshold
+        upper_threshold {float} -- upper threshold
+        year {int} -- year to check for average CO2 emmission
+    
+    Returns:
+        pyplot -- plot of every country inbetween the threshold.
+    """
+    plt.close()
+    year_str = str(year)
 
-    print (temp)
+    countries = co2_by_country[["Country Code", year_str]]
+    countries.loc[year_str] = pd.to_numeric(countries[year_str])
+    countries = countries[(countries[year_str] >= lower_threshold) & (countries[year_str] <= upper_threshold)]
+    
+    ax = countries.plot.bar(label='Normal', title="emissions")
+    ax.set(xlabel="Country code", ylabel="Emission per capita")
+
+    # adjust the plot, so the axis labels are visible
+    plt.gcf().subplots_adjust(bottom=0.15)
 
 
 
+    return plt
 
-
-def is_file(filename):
-    """Checks if a path is an actual directory"""
-    if not os.path.isfile(filename):
-        msg = "{0} is not a directory".format(filename)
-        raise argparse.ArgumentTypeError(msg)
-    else:
-        return filename
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Plots time vs. CO2 or time vs. temperature')
+    lower = 0.
+    upper = .2
+
+    year = 2000
+
+    plot_CO2_by_Country(lower, upper, year)
+
+
+# def is_file(filename):
+#     """Checks if a path is an actual directory"""
+#     if not os.path.isfile(filename):
+#         msg = "{0} is not a directory".format(filename)
+#         raise argparse.ArgumentTypeError(msg)
+#     else:
+#         return filename
+
+
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser(
+#         description='Plots time vs. CO2 or time vs. temperature')
  
-    parser.add_argument('time', help='''A csv file of time''')
-    parser.add_argument('compare_val', help='''A csv file of either CO2 levels or temperature''')
+#     parser.add_argument('time', help='''A csv file of time''')
+#     parser.add_argument('compare_val', help='''A csv file of either CO2 levels or temperature''')
 
-    args = parser.parse_args()
+#     args = parser.parse_args()
 
-    is_file(args.time)
+#     is_file(args.time)
---
+++ assignment6/web_visualization.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, request
+import temperature_CO2_plotter as tp
+from wtforms import SelectField
+
+app = Flask(__name__)
+
+
+@app.after_request
+def disable_caching(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    return response
+
+@app.route("/")
+def main_menu():
+    select = request.values.get('years_co2')
+    print("2")
+    print(str(select)) # just to see what select is
+    return render_template('home.html', test='static\co2.jpg')
+
+
+#co2 plotting
+@app.route('/plot_co2', methods=['POST'])
+def draw_co2():
+   year_from = int(request.form["year_from"])
+   year_to = int(request.form["year_to"])
+
+   y_min = int(request.form["min y-axis"])
+   y_max = int(request.form["max y-axis"])
+
+   if y_min > y_max or year_from > year_to:
+      return view_co2()
+
+   plot = tp.plot_CO2(year_from, year_to, y_min, y_max)
+   plot.savefig("static\images\co2.png", dpi = 200)
+
+   return view_co2(show = True)
+
+
+
+@app.route('/plot_co2')
+def view_co2(show=False):
+   if show:
+      return render_template("plot_co2.html", test='\static\images\co2.png')
+   else:
+      return render_template("plot_co2.html", test = None)
+
+
+#temp plotting
+@app.route('/plot_temp', methods=['POST'])
+def draw_temp():
+   year_from = int(request.form["year_from"])
+   year_to = int(request.form["year_to"])
+
+   month = request.form["month"]
+
+   y_min = int(request.form["min y-axis"])
+   y_max = int(request.form["max y-axis"])
+
+   if y_min > y_max or year_from > year_to:
+      return view_temp()
+
+   plot = tp.plot_temperature(month, year_from, year_to, y_min, y_max)
+   plot.savefig("static\images\\temp.png", dpi=200)
+
+   return view_temp(show=True)
+
+
+@app.route('/plot_temp')
+def view_temp(show = False):
+   if show:
+      return render_template("plot_temp.html", test='static\images\\temp.png')
+   else:
+      return render_template("plot_temp.html", test=None)
+
+if __name__ == "__main__":
+    app.run(debug=True)
